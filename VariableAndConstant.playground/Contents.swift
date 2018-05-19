02364d1e88e4b8b22b8c55432fc9492231e9fdc4
MasterUNG <phrombutr@gmail.com>
2018-05-19 10:30:36 +0700

    Variable And Constant

---
+++ VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//การประกาสตัวแปร ที่สามารถแก้ไขค่าได้
+//Implicit คือการประกาศตัวแปร ที่ใช้ Value กำหนด Type Data
+var nameString = "Doramon"  // String Type
+var numberInt = 123         // Int Type
+var numberDou = 3.1416      // Double Type
+var statusBol = false
+
+nameString = "Nopita"
+numberInt = 555
+
+//การกำหนดค่าคงที่
+let surnameString = "Phrumbutr"
+
+//surnameString = "NewPrhombutr" ไม่สามารถ Compile ได้
+let numInt = 100
+//numInt = 200
+
+//Explicit คือการประกาศตัวแปร แบบ กำหนด Data Type ได้
+var name2String: String = "Master"
+var num2Int: Int = 123
+
+
+
+
+
+
+
+
+
+
+
+
+
+
