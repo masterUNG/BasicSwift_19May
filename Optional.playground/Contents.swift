9320ca6fcbcd7b17791ddaebaa041e88dcdcb999
MasterUNG <phrombutr@gmail.com>
2018-05-19 13:53:57 +0700

    Learn Optional

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var numberInt: Int?
+
+
+
+var numberString = "6"
+
+   numberInt = Int(numberString)
+
+print("numberInt ==> \(numberInt)")
+
+var answerInt = numberInt! * 5
+print("Answer ==> \(answerInt)")
+
+
+
