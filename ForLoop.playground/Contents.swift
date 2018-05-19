c87360d0513d2479bc392a6eadd3ff4131e8e187
MasterUNG <phrombutr@gmail.com>
2018-05-19 15:19:43 +0700

    Learning For Loop and While Loop

---
+++ ForLoop.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var friendString = ["Doramon", "Nopita", "Sunako", "Chizuka"]
+
+for myFriend in friendString {
+    print("myFriend ==> \(myFriend)")
+}
+
+var numberInt = 1
+while numberInt <= 10 {
+    print("Times ==> \(numberInt)")
+    numberInt = numberInt + 1
+}
+
+
+
